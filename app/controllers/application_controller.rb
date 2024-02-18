e3d63735507bc4c939efad06278183b33164d9d3
Thomas Smith <tsmith17@sheffield.ac.uk>
2024-02-18 17:33:40 +0000

    Dashboard added. Redirect to dashboard if logged in at route added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :type])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :type])
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
+  before_action :redirect_if_authenticated
 
-  def home
+  def index
+  end
+
+  private
+
+  def redirect_if_authenticated
+    redirect_to dashboard_path if user_signed_in?
   end
 
 end
