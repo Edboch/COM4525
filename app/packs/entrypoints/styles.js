3fe73c00bc450f472419ee2d86d35cc5c770fcb9
Thomas Smith <tsmith17@sheffield.ac.uk>
2024-02-18 13:34:17 +0000

    Font added, home changed to login

--- app/packs/entrypoints/styles.js
+++ app/packs/entrypoints/styles.js
@@ -3,4 +3,4 @@ require.context('../images', true);
 import 'bootstrap-icons/font/bootstrap-icons';
 import '../styles/twitter_bootstrap';
 import '../styles/layout';
-import '../styles/home.scss';
+import '../styles/login.scss';
