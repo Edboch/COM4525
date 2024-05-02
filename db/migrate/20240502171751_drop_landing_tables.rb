dc80e6ba29b55990676b08a52768c720a76cec64
MiniEggz <owenscook1@gmail.com>
2024-05-02 18:21:12 +0100

    Remove all landing page db leftovers.

---
+++ db/migrate/20240502171751_drop_landing_tables.rb
@@ -0,0 +1,11 @@
+class DropLandingTables < ActiveRecord::Migration[7.1]
+  def change
+    drop_table :like_answers
+    drop_table :like_reviews
+    drop_table :reviews
+    drop_table :landing_page_views
+    drop_table :landing_visitor_locations
+    drop_table :landing_pages
+    drop_table :landing_users
+  end
+end
