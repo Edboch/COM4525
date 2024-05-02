e02bde1710a4f0d10fbe5bfa2f86d0cec9771960
Thomas Smith <tsmith17@sheffield.ac.uk>
2024-05-02 17:29:00 +0100

    Integrate player ratings with UI changes

--- app/decorators/match_decorator.rb
+++ app/decorators/match_decorator.rb
@@ -3,4 +3,14 @@
 # Decorator for matches
 class MatchDecorator < ApplicationDecorator
   delegate_all
+
+  # returns the displayable title for the match
+  # e.g. 'Aston Villa 2-1 Brighton'
+  def match_header
+    if object.start_time > Time.current
+      return "#{object.team.name} v #{object.opposition}"
+    end
+
+    "#{object.team.name} #{object.goals_for}-#{object.goals_against} #{object.opposition}"
+  end
 end
