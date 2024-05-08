55eb0803f51a921cdeb08c575186cec820441e9f
MiniEggz <owenscook1@gmail.com>
2024-05-08 03:25:35 +0100

    Update styling of forms and other mobile styling fixes.

--- app/packs/entrypoints/styles.js
+++ app/packs/entrypoints/styles.js
@@ -4,4 +4,4 @@ import 'bootstrap-icons/font/bootstrap-icons';
 import '../styles/twitter_bootstrap';
 import '../styles/layout';
 import '../styles/login.scss';
-import '../styles/main_styles.scss'
+import '../styles/main_styles.scss';
