5d7755185bc577e68f4aad943aa14e398ac26b17
Haoming Liu <hliu90@sheffield.ac.uk>
2024-05-09 11:09:49 +0100

    rubocop compliance

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -47,7 +47,9 @@ class MatchesController < ApplicationController
     @match.team_id = @team.id
 
     if @match.save
-      UserMailer.create_match_email(UserTeam.where(team_id: @team.id, accepted: true).map { |user_team| User.find_by(id: user_team.user_id) }).deliver
+      UserMailer.create_match_email(UserTeam.where(team_id: @team.id, accepted: true).map do |user_team|
+                                      User.find_by(id: user_team.user_id)
+                                    end).deliver
       redirect_to team_fixtures_path(@team.id), notice: I18n.t('match.create')
     else
       render :new, status: :unprocessable_entity
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
+# Mailer for sending email notifications
 class UserMailer < ApplicationMailer
   default from: 'PitchWize <no-reply@sheffield.ac.uk>'
 
   def create_match_email(user)
     @user = user
-    mail(to: 'no-reply@sheffield.ac.uk', bcc: @user.pluck(:email), subject: 'New Match Created')
+    mail(to: 'no-reply@sheffield.ac.uk', bcc: @user.pluck(:email), subject: I18n.t('mailer.new_match_created_subject'))
   end
-
 end
