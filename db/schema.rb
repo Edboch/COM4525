bec6590e279c46d1d159fa747cf6fb41aa6ed88f
Lingxi Zhang <lzhang122@sheffield.ac.uk>
2024-05-10 03:13:59 +0100

    Add test for removing team from a team's dedicated page as an admin

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_185749) do
     t.index ["team_id"], name: "index_invites_on_team_id"
   end
 
-  create_table "landing_viewers", force: :cascade do |t|
-    t.bigint "landing_user_id"
-    t.integer "selected_plan", default: 0, null: false
-    t.index ["landing_user_id"], name: "index_landing_viewers_on_landing_user_id"
-  end
-
   create_table "match_events", force: :cascade do |t|
     t.bigint "match_id", null: false
     t.bigint "user_id", null: false
@@ -69,17 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_185749) do
     t.index ["team_id"], name: "index_matches_on_team_id"
   end
 
-  create_table "page_to_page_step_counts", force: :cascade do |t|
-    t.bigint "landing_page_id_from", null: false
-    t.bigint "landing_page_id_to", null: false
-    t.integer "count", default: 0, null: false
-  end
-
-  create_table "penultimate_page_counts", force: :cascade do |t|
-    t.bigint "landing_page_id", null: false
-    t.integer "count", default: 0, null: false
-  end
-
   create_table "player_matches", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "match_id", null: false
@@ -101,15 +84,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_185749) do
     t.index ["user_id"], name: "index_player_ratings_on_user_id"
   end
 
-  create_table "question_answers", force: :cascade do |t|
-    t.string "question", null: false
-    t.string "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "show", default: false, null: false
-    t.integer "clicks", default: 0, null: false
-  end
-
   create_table "reports", force: :cascade do |t|
     t.bigint "user_id"
     t.text "content"
@@ -129,7 +103,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_185749) do
 
   create_table "site_admins", force: :cascade do |t|
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_site_admins_on_user_id"
+    t.index ["user_id"], name: "index_site_admins_on_user_id", unique: true
   end
 
   create_table "site_visit_groupings", force: :cascade do |t|
@@ -203,6 +177,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_185749) do
   add_foreign_key "player_matches", "users"
   add_foreign_key "player_ratings", "matches"
   add_foreign_key "player_ratings", "users"
+  add_foreign_key "site_admins", "users"
   add_foreign_key "team_activities", "teams"
   add_foreign_key "user_teams", "teams"
   add_foreign_key "user_teams", "users"
